d0dcdee03043fffb7cdcc8ae63f97812a1f0cae4
Sankalp-G <sankalp.gawali@gmail.com>
2022-12-09 18:30:05 +0530

    add random integer solution

---
+++ 13_rand_int/rand_int.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    // argument handling
+    if (argc != 2) {
+        printf("Invalid argument count\n");
+        printf("Usage: executable <amount_of_random_numbers>\n");
+        return -1;
+    }
+
+    int amount_of_nums = atoi(argv[1]);
+    if (amount_of_nums <= 0) {
+        printf("Argument must be a positive integer");
+        return -2;
+    }
+
+    printf("%d random integers are: \n", amount_of_nums);
+
+    for (int i = 0; i < amount_of_nums; i++) {
+        printf("%d\n", rand());
+    }
+}
